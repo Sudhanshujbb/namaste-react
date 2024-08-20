147706af596b7d7eb8e42472f7d98d10251f4286
Sudhanshu Maurya <sudhanshujbb@gmail.com>
2024-08-21 00:10:53 +0530

    Episode -02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 const heading = React.createElement("div", {id: "parent"}, [React.createElement("div", {id: "child1"}, [React.createElement("h1", {id: "heading"}, "I am H1"), React.createElement("h2", {id: "heading2"}, "I am H2")], React.createElement("div", {id: "child1"}, [React.createElement("h1", {id: "heading"}, "I am H1"), React.createElement("h2", {id: "heading2"}, "I am H2")]))]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
